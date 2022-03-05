0fe87fb0c7394af246771b7231c72f331733e6fc
Abhishek gupta <70539485+Tesla-gamer@users.noreply.github.com>
2022-03-06 00:29:39 +0530

    intial commit

---
+++ app/src/main/java/com/example/modulecheck/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.modulecheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+    ChipNavigationBar chipNavigationBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chipNavigationBar = findViewById(R.id.bottom_nav_bar);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_home,
+                true);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+        bottomMenu();
+    }
+
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener
+                (new ChipNavigationBar.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(int i) {
+                        Fragment fragment = null;
+                        switch (i) {
+                            case R.id.bottom_nav_home:
+                                fragment = new HomeFragment();
+                                break;
+                            case R.id.bottom_nav_book:
+                                fragment = new BookFragment();
+                                break;
+                            case R.id.bottom_nav_user:
+                                fragment = new UserFragment();
+                                break;
+                        }
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container,
+                                        fragment).commit();
+                    }
+                });
+
+    }
+}
